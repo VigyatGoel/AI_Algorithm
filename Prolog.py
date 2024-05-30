c69fc672cbec3643c2ba3e1d9d1e1392d4556915
VigyatGoel <vigyatgoel@gmail.com>
2024-05-30 10:37:11 +0530

    prolog

---
+++ Prolog.py
@@ -0,0 +1,12 @@
+professor(x,y) :- teach(x,c), study(y, c).
+study(nitin csc135).
+study(sumit, csc135).
+study(ravi, csc131).
+study(harsh, csc134).
+teach(sondhya, csc135).
+teach(rajendra, csc131).
+teach(ashish, csc171).
+teach(pritam, csc134).
+
+
+#study(nitin, What).
